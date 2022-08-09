95de52a0465214f3370c4bd02b458e889707cb06
Doris Okodugha <106775455+digitalDoryx@users.noreply.github.com>
2022-08-09 13:11:30 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -51,9 +51,9 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
+}
 
 /**
- *
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
